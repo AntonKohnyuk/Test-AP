06450c75f4f36f835d1b94eace24dfb2708e1287
Anton Kohnyuk <anton.kohnyuk@mail.ru>
2024-03-31 15:37:56 +0300

    feat:add routes and reset styles

--- check-weather/src/app/app.routes.ts
+++ check-weather/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { WeatherPageComponent } from './pages/weather-page/weather-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'weather-forecast', component: WeatherPageComponent },
+  { path: '**', redirectTo: '/weather-forecast' },
+];
