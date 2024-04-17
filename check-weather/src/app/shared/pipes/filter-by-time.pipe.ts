56f859adad02d1169ef0a42fc5dc177115c2049a
Anton Kohnyuk <anton.kohnyuk@mail.ru>
2024-04-17 23:59:28 +0300

    feat: add sync with current time

---
+++ check-weather/src/app/shared/pipes/filter-by-time.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { HourDataInterface } from '../types/hourly-forecast.interfaces';
+
+@Pipe({
+  name: 'filterByTime',
+  standalone: true,
+})
+export class FilterByTimePipe implements PipeTransform {
+  transform(value: HourDataInterface[]): HourDataInterface[] {
+    if (!value) return value;
+    const time = new Date();
+    console.log(value);
+    return value.filter(el => {
+      const elTime = new Date(el.time_epoch * 1000);
+      if (elTime.getDay() !== time.getDay()) return true;
+      else if (elTime.getHours() >= time.getHours()) return true;
+      else return false;
+    });
+  }
+}
