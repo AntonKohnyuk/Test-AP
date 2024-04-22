286eb51da3eca25eb0f346a54d04e83df54f12c3
Anton Kohnyuk <anton.kohnyuk@mail.ru>
2024-04-23 00:15:29 +0300

    fix: fix animation

--- check-weather/src/app/shared/components/daily-forecast/daily-forecast.component.ts
+++ check-weather/src/app/shared/components/daily-forecast/daily-forecast.component.ts
@@ -26,10 +26,6 @@ import { trigger, transition, style, animate } from '@angular/animations';
         style({ opacity: 0 }),
         animate('1000ms', style({ opacity: 1 })),
       ]),
-      transition(':leave', [
-        style({ opacity: 1 }),
-        animate('500ms', style({ opacity: 0 })),
-      ]),
     ]),
   ],
 })
